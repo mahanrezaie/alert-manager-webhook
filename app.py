93cbf5fab3986938700c5538f0df0afd3c975dc7
mahan <babremeshki@mahan-macbook.local>
2025-04-28 17:23:53 +0330

    [Update] app.py - health

--- app.py
+++ app.py
@@ -80,5 +80,9 @@ def webhook():
 
     return jsonify({"message": "Alerts processed successfully"}), 200
 
+@app.route("/health", methods=["GET"])
+def health():
+    return jsonify({"status": "ok"}), 200
+
 if __name__ == "__main__":
     app.run(debug=True)          
